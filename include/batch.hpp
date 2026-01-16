2e69e6f3f0b390701ec3ce5b898efdc6d9ca5e91
Belfer <mike@alonica.net>
2026-01-16 08:45:29 +0100

    Implemented variations on batching

--- include/batch.hpp
+++ include/batch.hpp
@@ -8,6 +8,14 @@ struct batch_t
 	batch_t(u32 prim_size, u32 capacity, u32 max_drawcalls, u32 buffer_count);
 	~batch_t();
 
+	// nocopy
+	batch_t(const batch_t&) = delete;
+	batch_t& operator=(const batch_t&) = delete;
+
+	// nomove
+	batch_t(batch_t&&) = delete;
+	batch_t& operator=(batch_t&&) = delete;
+
 	void begin();
 	void add_vertices(u8* data, u32 size);
 	void end();
@@ -19,6 +27,8 @@ struct batch_t
 	inline GLuint get_vbo() const { return vbo; }
 
 private:
+	bool updating{ false };
+
 	u32 prim_size{ 0 };
 	u32 capacity{ 0 };
 	u32 max_drawcalls{ 0 };
@@ -31,6 +41,5 @@ private:
 
 	u32 drawcount{ 0 };
 	u32 buffer_count{ 0 };
-	GLint* first{ nullptr };
-	GLsizei* count{ nullptr };
+	struct drawcalls_t* drawcalls{ nullptr };
 };
